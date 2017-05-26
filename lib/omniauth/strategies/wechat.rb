657b9acef1980fcc5e5853bcd7d88842d65a61bd
Li lin <yjh521@gmail.com>
2017-05-27 00:50:53 +0800

    Update wechat.rb

--- lib/omniauth/strategies/wechat.rb
+++ lib/omniauth/strategies/wechat.rb
@@ -65,7 +65,7 @@ module OmniAuth
           'code' => request.params['code'],
           'grant_type' => 'authorization_code'
           }.merge(token_params.to_hash(symbolize_keys: true))
-        client.get_token(params, deep_symbolize(options.auth_token_params || {}))
+        client.get_token(params, deep_symbolize(options.auth_token_params))
       end
     end
   end
